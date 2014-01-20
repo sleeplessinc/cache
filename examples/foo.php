bad449814df67ec0945423741bc28c8558d8fb93
joe <joe@sleepless.com>
2014-01-20 03:04:24 -0800

    -

--- examples/foo.php
+++ examples/foo.php
@@ -1,65 +1,97 @@
-<?
+<?php
 
-require_once "json.php";		// json2obj and obj2json functions
+	header('Content-type: text/plain');
+	header("Cache-Control: No-cache");
 
-$cacheHost = 'http://localhost:3000/';
+	require_once "dbg.php";
+	require_once "lib.php";
+	require_once "db.php";
+
+	getconfig();
+	$game_weeks = $config["game_weeks"];
 
 
-function cache( $url, $rawOpts ) {
-	$opts = array();
-	foreach( $rawOpts as $key => $val ) {
-		$opts[] = sprintf( '%s=%s', urlencode( $key ), urlencode( $val ) );
+	$most = 0.75;
+	//$most = 0;
+
+	// fetch the list of teachers
+	$sql = "select id, teacher_code, first_name, last_name, auth_count, email  from users where active=1 and user_type='t'";
+	$rs = dbExec($sql);
+	while($o = $rs->FetchNextObj())
+	{
+		$status = "No Students";	// Teacher registered but did not input students
+
+		$id = $o->id;
+		$first_name = $o->first_name;
+		$last_name = $o->last_name;
+		$full_name = "$last_name, $first_name";
+		$ac = $o->auth_count;
+		$email = $o->email;
+		$tc = $o->teacher_code;
+
+		$in_tutorial = 0;
+
+		// count how many students this teacher has
+		$sql = "select count(id) from users where active=1 and user_type='s' and teacher_code=$tc";
+		$num_students = dbGetFld($sql);
+		if($num_students > 0) {
+			// there's at least one student
+			
+			$status = "With Students";
+
+			// count how many of the students are assigned to a group (team)
+			$sql = "select count(id) from users where active=1 and user_type='s' and teacher_code=$tc and group_id != 0";
+			$num_in_group = dbGetFld($sql);
+
+			if($num_in_group / $num_students > $most) {
+				// most of this teacher's students are in a group
+				$status = "Roles and Teams";
+			}
+
+			// count how many groups this teacher has
+			$sql = "select count(id) from `groups` where teacher_code=$tc";
+			$num_groups = dbGetFld($sql);
+
+			// count how many groups are in the tutorial
+			$sql = "select count(id) from `groups` where teacher_code=$tc and week=0";
+			$num_groups_in_tutorial = dbGetFld($sql);
+			if($num_groups_in_tutorial > 0) {
+				// at least one group is still in tutorial
+				$in_tutorial = 1;
+			}
+
+			if($num_groups > 0) {
+				// this teacher has at least one group 
+
+				for($w = 1; $w <= ($game_weeks + 1); $w++) {
+
+					// count how many groups are in, or beyond, week $w
+					$sql = "select count(id) from groups where teacher_code=$tc and week>=$w";
+					$groups_in_week = dbGetFld($sql);;
+
+					if(($groups_in_week / $num_groups) > $most) {
+						// most of the groups are in, or beyond, week $w
+
+						$status = "Task $w";
+
+						if($w > $game_weeks) {
+							// this week is the last one
+							$status = "Finished";
+						}
+					}
+				}
+			}
+		}
+
+
+		// XXX task_progress was not a useful value
+		$sql = "update users set status = '$status', task_progress=0, in_tutorial=$in_tutorial where id = $id limit 1";
+		dbExec($sql);
+
+
+		echo "$tc\t$full_name\t<$email>\t$status\t$in_tutorial\n";
+		ob_flush();
+		flush();
 	}
-	$uri = $url . '?' . implode( '&', $opts );
-	$ch = curl_init( $uri );
-	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-	$r = curl_exec( $ch );
-	if($r === false) {
-		//echo "ERROR: cache server unavailable\n";
-		return null;
-	}
-	$o = json2obj($r);
-	if($o->error) {
-		//echo "ERROR: $o->error\n";
-		return null;
-	}
-	return $o->value;
-}
-
-function cacheSet($k, $o, $ttl = 0) {
-	global $cacheHost;
-	$v = obj2json($o);
-	$r = cache( $cacheHost, array(
-		'act' => 'set',
-		'key' => $k,
-		'val' => obj2json($v),
-		'ttl' => (int)$ttl
-		));
-	//echo "(cacheSet($k, $v) returning $r)\n";
-	return $r;
-}
-
-function cacheGet($k) {
-	global $cacheHost;
-	$v = cache( $cacheHost, array( 'act' => 'get', 'key' => $k ) );
-	//echo "(cacheGet($k) returning ".($v === null ? "null" : $v).")\n";
-	return $v;
-}
-
-
-if(true) {
-
-	// test code
-
-	$cc = "(from cache)";
-	$v = cacheGet("foo");
-	if($v == null) {
-		$v = 15;
-		$cc = "";
-		cacheSet("foo", $v, 10);
-	}
-
-	echo "v is $v $cc\n";
-}
 
 ?>
