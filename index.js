7163174c44adbf8628e8eb30b3b1e3e8f39d9900
Joe Hitchens <joe@sleepless.com>
2020-03-30 14:08:05 -0700

    -

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 
 // Copyright 2017 Sleepless Software Inc. All rights reserved.
 
+DS = require("ds").DS;
+
 function Cache(ttl) {
 
 	var me = this;
 
 	me.now = function() { return (new Date()).getTime() }
 	me.ttl = ttl || 0;
-	me.data = {}
+	//me.data = {}
+	me.data = new DS();
 
-	me.get = function(key) {
+	me.get = function(key, cb) {
 		var val = null
 		var obj = me.data[key]
 		if(obj) {
@@ -19,23 +22,34 @@ function Cache(ttl) {
 				delete me.data[key]
 			}
 		}
+		if(cb)
+			cb(val);
 		return val
 	}
 
-	me.del = function(key) {
+	me.del = function(key, cb) {
 		var oldval = me.get(key); 
 		delete me.data[key]
+		if(cb)
+			cb(oldval)
 		return oldval
 	}
 
-	me.put = function(key, val, ttl) {
+	me.put = function(key, val, ttl, cb) {
 		if(ttl === undefined) {
 			ttl = me.ttl;
 		}
 		var oldval = me.del(key); 
 		if(val !== null)
 			me.data[ key ] = { expires: me.now() + ttl, val: val }
+		if(cb)
+			cb(oldval)
 		return oldval
+	}
+
+	me.save = function(file_path, cb) {
+		me.data.save(file_path);
+		cb();
 	}
 
 }
